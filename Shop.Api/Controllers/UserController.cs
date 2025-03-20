76a3613255686dddb1096205b2e1db93e1dabc21
Mahdi Ebrahimi <mahdi78eb@gmail.com>
2025-03-20 23:46:17 +0330

    setup user api controller

---
+++ Shop.Api/Controllers/UserController.cs
@@ -0,0 +1,98 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Application.UserAgg.AddAddress;
+using Shop.Application.UserAgg.Create;
+using Shop.Application.UserAgg.Edit;
+using Shop.Application.UserAgg.EditAddress;
+using Shop.Application.UserAgg.Remove;
+using Shop.Application.UserAgg.RemoveAddress;
+using Shop.Query.Users.GetAddressById;
+using Shop.Query.Users.GetAddressList;
+using Shop.Query.Users.GetUserById;
+using Shop.Query.Users.GetUserList;
+
+namespace Shop.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult> GetUserList()
+        {
+            var query = await _mediator.Send(new GetUserLIstQuery());
+            return Ok(query);
+        }
+
+        [HttpGet("[action]/{userId:long}")]
+        public async Task<ActionResult> GetUserById(long userId)
+        {
+            var query = await _mediator.Send(new GetUserByIdQuery(userId));
+            return Ok(query);
+        }
+
+        [HttpPost("[action]")]
+        public async Task<ActionResult> AddUser(CreateUserCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok();
+        }
+
+        [HttpPut("[action]")]
+        public async Task<ActionResult> EditUser(EditUserCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok();
+        }
+
+        [HttpDelete("[action]/{userId:long}")]
+        public async Task<ActionResult> RemoveUserById(long userId)
+        {
+            await _mediator.Send(new RemoveUserCommand(userId));
+            return Ok();
+        }
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult> GetAddressListByUserId(long userId)
+        {
+            var query = await _mediator.Send(new GetAddressListQuery(userId));
+            return Ok(query);
+        }
+
+        [HttpGet("[action]/{addressId:long}")]
+        public async Task<ActionResult> GetAddressById(long addressId)
+        {
+            var query = await _mediator.Send(new GetAddressByIdQuery(addressId));
+            return Ok(query);
+        }
+
+        [HttpPost("[action]")]
+        public async Task<ActionResult> AddAddress(AddAddressCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok();
+        }
+
+        [HttpPut("[action]")]
+        public async Task<ActionResult> EditAddress(EditAddressCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok();
+        }
+
+        [HttpDelete("[action]/{userId:long}/{addressId:long}")]
+        public async Task<ActionResult> RemoveAddressById(long userId, long addressId)
+        {
+            await _mediator.Send(new RemoveAddressCommand(userId, addressId));
+            return Ok();
+        }
+    }
+}
---
+++ Shop.Infra/Migrations/20250320200650_init_User.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shop.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class init_User : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "User");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                schema: "User",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NationalCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProfileImage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BankCardNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                schema: "User",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    ReceiverName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReceiverPhone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReceiverProvince = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReceiverCity = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReceiverPostalAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReceiverPostalCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => new { x.UserId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_Addresses_Users_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "User",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Addresses",
+                schema: "User");
+
+            migrationBuilder.DropTable(
+                name: "Users",
+                schema: "User");
+        }
+    }
+}
